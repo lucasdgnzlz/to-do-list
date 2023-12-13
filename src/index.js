2d80a01f528895798ceab13cc83a8e550d6ad3fd
Lucas <lucasdgonzalez@outlook.com>
2023-12-12 22:34:24 -0300

    Agrega función para mostrar la lista activa

--- src/index.js
+++ src/index.js
@@ -269,6 +269,7 @@ function gestionarCambioDeLista (listaSeleccionada) {
 		$listaTareasCompletas.id = "oculto";
 		const $listaTareasTotales = document.querySelector(".contenedor-lista-tareas-totales");
 		$listaTareasTotales.id = "oculto";
+		mostrarListaActiva(listaSeleccionada);
 	} else if(listaSeleccionada === "Completas"){
 		const $listaTareasCompletas = document.querySelector(".contenedor-lista-tareas-completas");
 		$listaTareasCompletas.id = "";
@@ -277,6 +278,7 @@ function gestionarCambioDeLista (listaSeleccionada) {
 		$listaTareasPendientes.id = "oculto";
 		const $listaTareasTotales = document.querySelector(".contenedor-lista-tareas-totales");
 		$listaTareasTotales.id = "oculto";
+		mostrarListaActiva(listaSeleccionada);
 	} else if(listaSeleccionada === "Total") {
 		const $listaTareasTotales = document.querySelector(".contenedor-lista-tareas-totales");
 		$listaTareasTotales.id = "";
@@ -285,5 +287,22 @@ function gestionarCambioDeLista (listaSeleccionada) {
 		$listaTareasPendientes.id = "oculto";
 		const $listaTareasCompletas = document.querySelector(".contenedor-lista-tareas-completas");
 		$listaTareasCompletas.id = "oculto";
+		mostrarListaActiva(listaSeleccionada);
 	}
+}
+
+function mostrarListaActiva(listaSeleccionada) {
+	const $opcionesListasDeTareas = document.querySelectorAll(".texto-opcion");
+	
+	$opcionesListasDeTareas.forEach(($opcionListaTarea) => {
+		if($opcionListaTarea.classList.contains("opcion-activa")) {
+			$opcionListaTarea.classList.remove("opcion-activa");
+		}
+	});
+
+	$opcionesListasDeTareas.forEach(($opcionListaTarea) => {
+		if($opcionListaTarea.textContent === listaSeleccionada) {
+			$opcionListaTarea.classList.add("opcion-activa");
+		}
+	});
 }
