9e8aad372618958c2af461e32e8f1e862435b46c
Lucas <lucasdgonzalez@outlook.com>
2024-01-04 23:05:44 -0300

    Crea funciones para recrear las tareas de localStorage
    La función gestionarDataTareasGuardadas es la que se encarga de dividir las tareas guardadas en localStorage, y sus respectivas características para poder mandarlas a la función recrearTareasGuardadas, para que esta misma se encargue de recrear nuevamente las tareas que estaban guardadas.

--- src/index.js
+++ src/index.js
@@ -484,6 +484,7 @@ function iniciarPagina() {
 	try{
 		const tareasGuardadas = cargarTareasDeLocalStorage(DATA_A_BUSCAR);
 		enlistarTareasGuardadas(tareasGuardadas);
+		gestionarDataTareasGuardadas(tareasGuardadas);
 	} catch(e) {
 		return console.error(e);
 	}
@@ -494,4 +495,128 @@ function enlistarTareasGuardadas(dataTareas) {
 	dataTareas.forEach((tarea) => {
 		tareasGuardadasEnLocalStorage.push(tarea);
 	});
+}
+
+function gestionarDataTareasGuardadas(dataTareas){
+	dataTareas.forEach((tareaGuardada, i) => {
+		const listaClasesTareaGuardada = tareaGuardada[`tarea_${i + 1}`]["clasesTarea"];
+		const keysClasesTareaGuardada = Object.keys(listaClasesTareaGuardada); // esto es un objeto
+		const cantidadClasesTareaGuardada = keysClasesTareaGuardada.length;
+		const nombreTareaGuardada = tareaGuardada[`tarea_${i + 1}`]["nombreTarea"];
+		let listaALaQuePerteneceLaTareaGuardada;
+		let clasesTareaGuardada;
+
+		for(let i = 0; i < cantidadClasesTareaGuardada; i++){
+			if(clasesTareaGuardada === undefined){
+				clasesTareaGuardada = `${listaClasesTareaGuardada[i]} `;
+			} else{
+				clasesTareaGuardada += `${listaClasesTareaGuardada[i]} `;
+			}
+
+			if(listaClasesTareaGuardada[i] === "tarea-pendiente"){
+				listaALaQuePerteneceLaTareaGuardada = "lista-tareas-pendientes";
+			} else if(listaClasesTareaGuardada[i] === "tarea-completa"){
+				listaALaQuePerteneceLaTareaGuardada = "lista-tareas-completas";
+			}
+		}
+
+		recrearTareasGuardadas(listaALaQuePerteneceLaTareaGuardada, clasesTareaGuardada, nombreTareaGuardada);
+	});
+}
+
+function recrearTareasGuardadas(listaALaQuePerteneceLaTareaGuardada, clasesTareaGuardada, nombreTareaGuardada){
+	const $listaTareas = document.querySelector(`.${listaALaQuePerteneceLaTareaGuardada}`);
+
+	const nuevaTarea = document.createElement("li");
+	nuevaTarea.classList = clasesTareaGuardada;
+
+	const nuevoContenedorEstado = document.createElement("div");
+	nuevoContenedorEstado.className = "contenedor-estado-tarea";
+
+	const nuevoEstadoTarea = document.createElement("input");
+	nuevoEstadoTarea.className = "form-check-input estado-tarea";
+	nuevoEstadoTarea.type = "checkbox";
+	nuevoEstadoTarea.id = "flexCheckDefault";
+
+	if(listaALaQuePerteneceLaTareaGuardada === "lista-tareas-completas") {
+		nuevoEstadoTarea.checked = true;
+	}
+
+	const nuevoTituloTarea = document.createElement("p");
+	nuevoTituloTarea.className = "nombre-tarea";
+	nuevoTituloTarea.textContent = nombreTareaGuardada;
+
+	const nuevoContenedorOpciones = document.createElement("div");
+	nuevoContenedorOpciones.className = "contenedor-opciones-tarea";
+
+	const nuevoBotonOpciones = document.createElement("button");
+	nuevoBotonOpciones.className = "boton-opciones-tarea";
+	nuevoBotonOpciones.type="button dropdown-toggle";
+	nuevoBotonOpciones.setAttribute("data-bs-toggle", "dropdown");
+
+	const nuevoIconoBoton = document.createElement("i");
+	nuevoIconoBoton.className = "fa-solid fa-ellipsis icono-opciones-tareas";
+
+	const nuevaListaOpciones = document.createElement("ul");
+	nuevaListaOpciones.className = "dropdown-menu";
+
+	const nuevaOpcionPrioridadTres = document.createElement("li");
+	nuevaOpcionPrioridadTres.className = "bloque-opciones-tareas";
+
+	const nuevaPrioridadTres = document.createElement("div");
+	nuevaPrioridadTres.className = "opciones-tareas opcion-prioridad-3";
+	nuevaPrioridadTres.textContent = "Prioridad: 3";
+
+	const nuevaOpcionPrioridadDos = document.createElement("li");
+	nuevaOpcionPrioridadDos.className = "bloque-opciones-tareas";
+
+	const nuevaPrioridadDos = document.createElement("div");
+	nuevaPrioridadDos.className = "opciones-tareas opcion-prioridad-2";
+	nuevaPrioridadDos.textContent = "Prioridad: 2";
+
+	const nuevaOpcionPrioridadUno = document.createElement("li");
+	nuevaOpcionPrioridadUno.className = "bloque-opciones-tareas";
+
+	const nuevaPrioridadUno = document.createElement("div");
+	nuevaPrioridadUno.className = "opciones-tareas opcion-prioridad-1";
+	nuevaPrioridadUno.textContent = "Prioridad: 1";
+
+	const nuevoBloqueEliminarTarea = document.createElement("li");
+	nuevoBloqueEliminarTarea.className = "bloque-opciones-tareas";
+
+	const nuevoContenedorBorrarTarea = document.createElement("div");
+	nuevoContenedorBorrarTarea.className = "bloque-borrar-tarea";
+	
+	const nuevoIconoBorrarTarea = document.createElement("i");
+	nuevoIconoBorrarTarea.className = "fa-regular fa-trash-can borrar-tarea";
+
+	const nuevoTextoBorrarTarea = document.createElement("div");
+	nuevoTextoBorrarTarea.className = "opciones-tareas borrar-tarea";
+	nuevoTextoBorrarTarea.textContent = "Eliminar tarea";
+
+	nuevoContenedorEstado.appendChild(nuevoEstadoTarea);
+	nuevoContenedorEstado.appendChild(nuevoTituloTarea);
+	nuevaTarea.appendChild(nuevoContenedorEstado);
+
+	nuevoBotonOpciones.appendChild(nuevoIconoBoton);
+	nuevaOpcionPrioridadTres.appendChild(nuevaPrioridadTres);
+	nuevaOpcionPrioridadDos.appendChild(nuevaPrioridadDos);
+	nuevaOpcionPrioridadUno.appendChild(nuevaPrioridadUno);
+	nuevoContenedorBorrarTarea.appendChild(nuevoIconoBorrarTarea);
+	nuevoContenedorBorrarTarea.appendChild(nuevoTextoBorrarTarea);
+	nuevoBloqueEliminarTarea.appendChild(nuevoContenedorBorrarTarea);
+	
+	nuevaListaOpciones.appendChild(nuevaOpcionPrioridadTres);
+	nuevaListaOpciones.appendChild(nuevaOpcionPrioridadDos);
+	nuevaListaOpciones.appendChild(nuevaOpcionPrioridadUno);
+	nuevaListaOpciones.appendChild(nuevoBloqueEliminarTarea);
+
+	nuevoContenedorOpciones.appendChild(nuevoBotonOpciones);
+	nuevoContenedorOpciones.appendChild(nuevaListaOpciones);
+	nuevaTarea.appendChild(nuevoContenedorOpciones);
+
+	$listaTareas.appendChild(nuevaTarea);
+
+	cambiarColorNombreTareas(nuevaTarea);
+	cambiarColorIconoOpcionesTareas(nuevaTarea);
 }
