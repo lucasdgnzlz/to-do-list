5b96e5d40767a84db2bb1f2d53626cf99dcabe51
Lucas <lucasdgonzalez@outlook.com>
2024-02-28 20:46:21 -0300

    Sube prueba para guardar nueva tarea en localstorage

---
+++ src/storage/__tests__/to-do-list.spec.js
@@ -0,0 +1,30 @@
+/**
+ * @jest-environment jsdom
+ */
+
+/// <reference types="Jest" />
+
+import "jest-localstorage-mock";
+
+import { guardarTareasEnLocalStorage, cargarTareasDeLocalStorage, actualizarDatosTareaGuardada, eliminarTareaDeLocalStorage, eliminarTodasLasTareasDeLocalStorage } from "../to-do-list";
+import listasTareas1Fixture from "../../../cypress/fixtures/listasTareas1.fixture";
+import listasTareas3Fixture from "../../../cypress/fixtures/listasTareas3.fixture";
+
+describe("guardarTareasEnLocalStorage", () => {
+	beforeEach(() => {
+		localStorage.clear();
+	});
+
+	it("Prueba guardar datos de una nueva tarea en localStorage", () => {
+		document.body.innerHTML = listasTareas3Fixture;
+		const CONTEXTO_TAREA = "nueva tarea";
+		const NOMBRE_TAREA = "Hacer ejercicio";
+
+		const datosAComparar = [{tarea: {nombreTarea: "Hacer ejercicio", clasesTarea: {"0": "tarea", "1": "tarea-pendiente"}}}];
+		const NOMBRE_KEY_DATOS_LOCAL_STORAGE = "tareasGuardadas";
+
+
+		guardarTareasEnLocalStorage(CONTEXTO_TAREA, NOMBRE_TAREA);
+		expect(JSON.parse(localStorage.getItem(NOMBRE_KEY_DATOS_LOCAL_STORAGE))).toEqual(datosAComparar);
+	});
+});
